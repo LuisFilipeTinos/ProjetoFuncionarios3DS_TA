a19e99d58321760d4501524dcb8bba9ff183facd
LuisFilipeTinos <lftinos@gmail.com>
2026-07-30 14:35:39 -0300

    Primeiro commit

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoCadastroMVC.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoCadastroMVC.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public string? Cargo { get; set; }
+        public string? Departamento { get; set; }
+        public double Salario { get; set; }
+    }
+}
